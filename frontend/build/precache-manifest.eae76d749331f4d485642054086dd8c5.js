91654289c4c852dca48f3893f526340881dda35b
Ashwin Rao <54774413+ashwin400@users.noreply.github.com>
2021-07-04 21:29:36 +0530

    new Commit-2

---
+++ frontend/build/precache-manifest.eae76d749331f4d485642054086dd8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "178ad179ac3bbe68bb6e",
+    "url": "/static/css/main.cf97d26d.chunk.css"
+  },
+  {
+    "revision": "178ad179ac3bbe68bb6e",
+    "url": "/static/js/main.590edaab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06ea3ede61e9881125cc",
+    "url": "/static/js/2.5cf0e22f.chunk.js"
+  },
+  {
+    "revision": "d350ce249a30596db0ed98838873a907",
+    "url": "/static/media/Comfortaa-Regular.d350ce24.ttf"
+  },
+  {
+    "revision": "2ec7055a6d9daf38170117123ff9aead",
+    "url": "/index.html"
+  }
+];
